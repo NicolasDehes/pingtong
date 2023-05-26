7a16f6822868de8bba738dc024889963db8ba63b
florian <florian.toribio@gmail.com>
2023-05-26 12:01:16 +0200

    change header

---
+++ App.style.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  drawerContent: {
+    flex: 1,
+    marginTop: 20,
+  },
+  drawerItem: {
+    backgroundColor: 'rgba(27, 194, 0, 0.2)',
+  },
+  drawerLabel : {
+    color: '#383F39',
+    fontSize: 16,
+  },
+});
