05a39416eee7509e903a1d355d1a7cc0460eb50f
florian <florian.toribio@gmail.com>
2023-05-26 15:35:05 +0200

    firebase

--- components/Form/SignUp/SignUp.tsx
+++ components/Form/SignUp/SignUp.tsx
@@ -1,48 +1,61 @@
-import React from "react"
-import { useFormik } from "formik"
+import React from 'react';
+import { useFormik } from 'formik';
 
-import { ImageBackground, ScrollView, SafeAreaView } from "react-native"
+import { ImageBackground, ScrollView, SafeAreaView } from 'react-native';
 
-import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import validationSchema from "./validationSchema"
+import validationSchema from './validationSchema';
 
-import { LinearGradient } from 'expo-linear-gradient'
-import { Input, Icon, Button, Text } from "@rneui/themed"
+import { LinearGradient } from 'expo-linear-gradient';
+import { Input, Icon, Button, Text } from '@rneui/themed';
 
-import { styles } from "./SignUp.styles"
+import { styles } from './SignUp.styles';
+import { auth } from '../../../firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const initialValues = {
-    pseudo: "",
-    lastName: "",
-    firstName: "",
-    email: "",
-    password: "",
-    passwordConfirmation: "",
-}
+    pseudo: '',
+    lastName: '',
+    firstName: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+};
 
 export default function SignUp(navigation: any) {
-    const insets = useSafeAreaInsets()
+    const insets = useSafeAreaInsets();
 
-    const onSubmit = (values: any) => {
-        // Submit
-        setTimeout(() => {
-            //navigation.navigate('')
-        }, 400)
-    }
+    const onSubmit = (values: any) => {     
+        createUserWithEmailAndPassword(auth, values.email, values.password)
+            .then((value) => {
+                console.log(value);
+                // navigation.navigate();
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
     const formikSignUp = useFormik({
         initialValues,
         validationSchema,
         onSubmit,
-    })
+    });
 
-    const { values, errors, handleChange, handleSubmit, isValid, isSubmitting } = formikSignUp
+    const {
+        values,
+        errors,
+        handleChange,
+        handleSubmit,
+        isValid,
+        isSubmitting,
+    } = formikSignUp;
 
     return (
         <ImageBackground
             style={styles.background}
-            source={require("../../../assets/images/fond-pingpong.png")}
+            source={require('../../../assets/images/fond-pingpong.png')}
             blurRadius={5}
         >
             <SafeAreaView
@@ -50,8 +63,8 @@ export default function SignUp(navigation: any) {
                     flex: 1,
                     marginHorizontal: 20,
                     marginVertical: 20,
-                    backgroundColor: "rgba(255, 255, 255, 0.5)",
-                    shadowColor: "black",
+                    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                    shadowColor: 'black',
                     shadowOffset: { width: 20, height: 20 },
                     shadowOpacity: 0.5,
                     borderRadius: 10,
@@ -62,23 +75,24 @@ export default function SignUp(navigation: any) {
                 <Text
                     h1
                     h1Style={{
-                        fontFamily: "Nunito-SemiBold",
-                        textAlign: "center",
+                        fontFamily: 'Nunito-SemiBold',
+                        textAlign: 'center',
                         marginBottom: 20,
                     }}
                 >
                     S'inscrire
                 </Text>
 
-                <ScrollView
-                    keyboardShouldPersistTaps="handled"
-                >
+                <ScrollView keyboardShouldPersistTaps="handled">
                     <Input
                         placeholder="Entrer un pseudonyme *"
                         label="Votre Pseudonyme"
-                        labelStyle={{ fontFamily: "Nunito-Regular", color: "#383F39" }}
+                        labelStyle={{
+                            fontFamily: 'Nunito-Regular',
+                            color: '#383F39',
+                        }}
                         leftIcon={<Icon name="person-outline" size={20} />}
-                        onChangeText={handleChange("pseudo")}
+                        onChangeText={handleChange('pseudo')}
                         value={values.pseudo}
                         placeholderTextColor="#383F39"
                         errorMessage={errors.pseudo}
@@ -87,9 +101,14 @@ export default function SignUp(navigation: any) {
                     <Input
                         placeholder="Entrer un nom"
                         label="Votre Nom"
-                        labelStyle={{ fontFamily: "Nunito-Regular", color: "#383F39" }}
-                        leftIcon={<Icon name="drive-file-rename-outline" size={20} />}
-                        onChangeText={handleChange("lastName")}
+                        labelStyle={{
+                            fontFamily: 'Nunito-Regular',
+                            color: '#383F39',
+                        }}
+                        leftIcon={
+                            <Icon name="drive-file-rename-outline" size={20} />
+                        }
+                        onChangeText={handleChange('lastName')}
                         placeholderTextColor="#383F39"
                         value={values.lastName}
                         errorMessage={errors.lastName}
@@ -98,9 +117,14 @@ export default function SignUp(navigation: any) {
                     <Input
                         placeholder="Entrer un prénom"
                         label="Votre prénom"
-                        labelStyle={{ fontFamily: "Nunito-Regular", color: "#383F39" }}
-                        leftIcon={<Icon name="drive-file-rename-outline" size={20} />}
-                        onChangeText={handleChange("firstName")}
+                        labelStyle={{
+                            fontFamily: 'Nunito-Regular',
+                            color: '#383F39',
+                        }}
+                        leftIcon={
+                            <Icon name="drive-file-rename-outline" size={20} />
+                        }
+                        onChangeText={handleChange('firstName')}
                         placeholderTextColor="#383F39"
                         value={values.firstName}
                         errorMessage={errors.firstName}
@@ -109,10 +133,13 @@ export default function SignUp(navigation: any) {
                     <Input
                         placeholder="Entrer un email *"
                         label="Votre adresse mail"
-                        labelStyle={{ fontFamily: "Nunito-Regular", color: "#383F39" }}
+                        labelStyle={{
+                            fontFamily: 'Nunito-Regular',
+                            color: '#383F39',
+                        }}
                         leftIcon={<Icon name="mail-outline" size={20} />}
                         placeholderTextColor="#383F39"
-                        onChangeText={handleChange("email")}
+                        onChangeText={handleChange('email')}
                         value={values.email}
                         errorMessage={errors.email}
                     />
@@ -120,10 +147,13 @@ export default function SignUp(navigation: any) {
                     <Input
                         placeholder="Entrer un mot de passe *"
                         label="Votre mot de passe"
-                        labelStyle={{ fontFamily: "Nunito-Regular", color: "#383F39" }}
+                        labelStyle={{
+                            fontFamily: 'Nunito-Regular',
+                            color: '#383F39',
+                        }}
                         leftIcon={<Icon name="lock-outline" size={20} />}
                         placeholderTextColor="#383F39"
-                        onChangeText={handleChange("password")}
+                        onChangeText={handleChange('password')}
                         value={values.password}
                         errorMessage={errors.password}
                     />
@@ -131,9 +161,12 @@ export default function SignUp(navigation: any) {
                     <Input
                         placeholder="Confirmer le mot de passe *"
                         label="Confirmation de votre mot de passe"
-                        labelStyle={{ fontFamily: "Nunito-Regular", color: "#383F39" }}
+                        labelStyle={{
+                            fontFamily: 'Nunito-Regular',
+                            color: '#383F39',
+                        }}
                         leftIcon={<Icon name="lock-outline" size={20} />}
-                        onChangeText={handleChange("passwordConfirmation")}
+                        onChangeText={handleChange('passwordConfirmation')}
                         placeholderTextColor="#383F39"
                         value={values.passwordConfirmation}
                         errorMessage={errors.passwordConfirmation}
@@ -142,7 +175,12 @@ export default function SignUp(navigation: any) {
 
                 <Button
                     disabled={!isValid || isSubmitting}
-                    disabledTitleStyle={{ fontFamily: "Nunito-Regular", textTransform: "uppercase", fontSize: 16, color: "#383F39" }}
+                    disabledTitleStyle={{
+                        fontFamily: 'Nunito-Regular',
+                        textTransform: 'uppercase',
+                        fontSize: 16,
+                        color: '#383F39',
+                    }}
                     loading={isSubmitting}
                     ViewComponent={LinearGradient}
                     linearGradientProps={{
@@ -150,8 +188,17 @@ export default function SignUp(navigation: any) {
                         start: { x: 0, y: 0 },
                         end: { x: 1, y: 1 },
                     }}
-                    style={{ marginHorizontal: 80, marginVertical: 2, marginTop: 20 }}
-                    titleStyle={{ fontFamily: "Nunito-Regular", textTransform: "uppercase", fontSize: 16, color: "#383F39" }}
+                    style={{
+                        marginHorizontal: 80,
+                        marginVertical: 2,
+                        marginTop: 20,
+                    }}
+                    titleStyle={{
+                        fontFamily: 'Nunito-Regular',
+                        textTransform: 'uppercase',
+                        fontSize: 16,
+                        color: '#383F39',
+                    }}
                     onPress={() => handleSubmit()}
                     title="S'inscrire"
                 />
@@ -159,10 +206,17 @@ export default function SignUp(navigation: any) {
                 <Button
                     title="Vous avez déjà un compte ?"
                     type="clear"
-                    titleStyle={{ fontFamily: "Nunito-Regular", color: "#383F39", textDecorationLine: "underline", fontSize: 18 }}
-                    onPress={() => navigation.navigation.navigate("Se connecter")}
+                    titleStyle={{
+                        fontFamily: 'Nunito-Regular',
+                        color: '#383F39',
+                        textDecorationLine: 'underline',
+                        fontSize: 18,
+                    }}
+                    onPress={() =>
+                        navigation.navigation.navigate('Se connecter')
+                    }
                 />
             </SafeAreaView>
         </ImageBackground>
-    )
+    );
 }
